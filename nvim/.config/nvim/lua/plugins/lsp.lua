ade5337b85c23f483f51ee51c059ba43bf70a4b8
Vadym Barabanov <introvert.png@gmail.com>
2024-12-12 12:04:51 +0200

    nvim: disable autosave for ts file; minor refactoring

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -3,37 +3,43 @@ return {
     config = function()
         local lsp = require("lspconfig")
 
+        local bufopts = { noremap = true, silent = true, buffer = buf }
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+
+        vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float(0, { scope = "line" }) end)
+        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
+        vim.keymap.set('n', '<leader>.', vim.lsp.buf.code_action)
+
+        vim.keymap.set('n', '<leader>d[', vim.diagnostic.goto_prev)
+        vim.keymap.set('n', '<leader>d]', vim.diagnostic.goto_next)
+
         local on_attach = function(client, bufnr)
-            -- if client.server_capabilities.documentFormattingProvider then 
-            --     local format = vim.api.nvim_create_augroup("Format", { clear = false })
-            --     vim.api.nvim_create_autocmd("BufWritePre", {
-            --         command = "silent! lua vim.lsp.buf.format()",
-            --         group = format,
-            --         buffer = bufnr,
-            --     })
-            -- end
-
-            local bufopts = { noremap = true, silent = true, buffer = buf }
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-
-            vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float(0, { scope = "line" }) end)
-            vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
-            vim.keymap.set('n', '<leader>.', vim.lsp.buf.code_action)
-
-            vim.keymap.set('n', '<leader>d[', vim.diagnostic.goto_prev)
-            vim.keymap.set('n', '<leader>d]', vim.diagnostic.goto_next)
+            if client.server_capabilities.documentFormattingProvider then 
+                local format = vim.api.nvim_create_augroup("Format", { clear = false })
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    command = "silent! lua vim.lsp.buf.format()",
+                    group = format,
+                    buffer = bufnr,
+                })
+            end
         end
 
-        local servers = { "ts_ls", "gopls", "zls" }
+        lsp["ts_ls"].setup({
+            -- TODO: I want diffrent formatters per project
+            -- on_attach = on_attach,
+            init_options = {
+                preferences = {
+                    importModuleSpecifierPreference = "relative", 
+                },
+            },
+        })
 
-        for _, server in ipairs(servers) do
-            lsp[server].setup({
-                on_attach = on_attach,
-            })
-        end
+        lsp["gopls"].setup({ on_attach = on_attach })
+
+        lsp["zls"].setup({ on_attach = on_attach })
     end,
 }
 
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,8 +2,19 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
+        local langs = {
+            "html",
+            "css",
+            "scss",
+            "javascript",
+            "typescript",
+            "tsx",
+            "go",
+            "zig", 
+        }
+
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "html", "css", "scss", "javascript", "typescript", "tsx", "go", "zig" },
+            ensure_installed = langs,
             highlight = { enable = true },
         })
     end,
